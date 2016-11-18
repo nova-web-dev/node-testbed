49052c50889df4f3dfce8c3079cfa8b048c65e17
Carl Eiserman <carleiserman@gmail.com>
2016-11-18 11:06:46 +0200

    Added all code back into index

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var async = require('async');
+var http = require('http');
+var bodyParser = require ("body-parser");
+
+var app = express();
+
+app.set('port', process.env.PORT || 8080);
+
+/** bodyParser.urlencoded(options)
+ * Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
+ * and exposes the resulting object (containing the keys and values) on req.body
+ */
+// app.use(bodyParser.urlencoded({
+//     extended: true
+// }));
+
+/**bodyParser.json(options)
+ * Parses the text as JSON and exposes the resulting object on req.body.
+ */
+app.use(bodyParser.json());
+
+app.post("/", function (req, res) {
+    console.log("====================================");
+    console.log("Received request - Printing Headers");
+    console.log("====================================");
+    console.log(req.headers)
+    console.log("====================================");
+    console.log("HEADERS DONE - PRINTING Body");
+    console.log("====================================");
+    console.log(req.body);
+
+    res.send("This is successful!");
+});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+ console.log('Express server listening on port ' + app.get('port'));
+});
